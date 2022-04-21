9d76301189b78b2093e267cc9120e2cd460afdd1
sunhey kim <jinxianhuijxh@naver.com>
2022-04-21 20:10:26 +0900

    layot

---
+++ app/src/main/java/kr/hs/emirim/mrim_0421/MainActivity.java
@@ -0,0 +1,26 @@
+package kr.hs.emirim.mrim_0421;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        LinearLayout rootLayout = new LinearLayout(this);
+        rootLayout.setOrientation(LinearLayout.VERTICAL);
+        rootLayout.setBackgroundColor(Color rgb(red:0, green:255, blue:0));
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT);
+        setContentView(rootLayout, params);
+        Button btn = new Button(this);
+        btn.setText("버튼연습");
+        rootLayout.addView(btn);
+    }
+}
